cc68637f362cb594dec9bc02e914fa605f5444c2
Lucca Giovane Gomes <lucca.giovane.gomes@uel.br>
2025-01-08 16:09:50 -0300

    Quase la

--- diario/urls.py
+++ diario/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('escrever/', views.escrever, name="escrever"),
+    path('cadastrar_pessoa/', views.cadastrar_pessoa, name="cadastrar_pessoa"),
+    path('dia/', views.dia, name='dia'),
+    path('excluir_dia', views.excluir_dia, name='excluir_dia')
 ]
--- diario/views.py
+++ diario/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Pessoa, Diario
+from datetime import datetime, timedelta
+
 def home(request):
-    return render(request, 'home.html')
+    textos = Diario.objects.all().order_by('-create_at')[:3]
+
+    return render(request, 'home.html', {'textos':textos})
 
 def escrever(request):
     if request.method == 'GET':
-        return render(request, 'escrever.html')
+        pessoas = Pessoa.objects.all()
+
+        return render(request, 'escrever.html', {'pessoas':pessoas})
+
     elif request.method == 'POST':
 
         titulo  = request.POST.get('titulo')
@@ -13,4 +21,53 @@ def escrever(request):
         pessoas = request.POST.getlist('pessoas')
         texto   = request.POST.get('texto')
 
-        return HttpResponse(f'{titulo} - {tags} - {pessoas} - {texto}')
+        if len(titulo.strip()) == 0 or len(texto.strip()) == 0:
+            #TODO: adicionar mensagens de erro
+            return redirect('escrever')
+
+        diario = Diario(titulo=titulo, texto=texto)
+        diario.save()
+
+        for i in pessoas:
+            pessoa = Pessoa.objects.get(id = i)
+            diario.pessoas.add(pessoa)
+
+        diario.set_tags(tags)
+        diario.save()
+
+        #TODO: adicionar msg de sucesso
+        return redirect('escrever')
+
+
+def cadastrar_pessoa(request):
+    if request.method == 'GET':
+        return render(request, 'pessoa.html')
+
+    elif request.method == 'POST':
+
+        nome = request.POST.get('nome')
+        foto = request.FILES.get('foto')
+
+        pessoa = Pessoa(nome=nome, foto=foto)
+        pessoa.save()
+
+        return redirect('escrever')
+
+def dia(request):
+    data = request.GET.get('data')
+    data_formatada = datetime.strptime(data, '%Y-%m-%d')
+
+    diarios = (Diario.objects.filter(create_at__gte=data_formatada)
+              .filter(create_at__lte=data_formatada + timedelta(days=1)))
+
+
+    return render(request, 'dia.html', {'diarios':diarios, 'total':diarios.count(), 'data':data})
+
+def excluir_dia(request):
+    dia = datetime.strptime(request.GET.get('data'),'%Y-%m-%d')
+    diarios = (Diario.objects.filter(create_at__gte=dia)
+               .filter(create_at__lte=dia + timedelta(days=1)))
+
+    diarios.delete()
+
+    return HttpResponse('teste')
